529c73934e114206c36bc0dcf78c4bb0aa4c8d48
aven <wangbiwen_88@qq.com>
2015-12-30 10:03:14 +0800

    create hello.py for test

---
+++ hello.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#########################################################################
+# File Name: hello.py
+# Author: Wang Biwen
+# mail: wangbiwen88@126.com
+# Created Time: 2015.12.29
+#########################################################################
+
+print "Hello, my name is Aven."
+
+def printHello():
+    print "hello"
+
+printHello()
